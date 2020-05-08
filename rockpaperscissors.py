44402fcf51aecb675927eea48d4f7cf6aec53cba
Henry Zhu <hi@henryzoo.com>
2020-05-08 17:36:35 -0400

    define a calculate winner function with boolean logic instead of using numbers

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -16,44 +16,55 @@ s-p=2, and p1==s, p1 wins
 import random
 import sys
 
-d = {"r": 0, "p": 2, "s": 4}
-hand = [0, 2, 4]
+hand = ['r', 'p', 's']
 cscor = 0  # computer score
 pscor = 0  # player score
 
-def computer_chooses():
-    comp_choice = random.choice(hand)
-    if comp_choice == 0:
-        print("computer: r")
-    elif comp_choice == 2:
-        print("computer: p")
-    elif comp_choice == 4:
-        print("computer: s")
-    return comp_choice
+# draw: rr, ss, pp
+# p1 wins: sp, rs, pr
+# c wins: ps, sr, rp
+
+def calculate_winner(player, computer):
+    if player == computer:
+        return "draw"
+    elif (computer == "r" and player == 's') or (computer == "p" and player == 'r') or (computer == "s" and player == 'p'):
+        return "computer"
+    else:
+        return "player"
 
 def loop():
     global cscor, pscor
-    p1_choice = d[input("player 1: ")]
-    comp_choice = computer_chooses()
+    p1_choice = input("player 1: ")
+    comp_choice = random.choice(hand)
+    print('computer:', comp_choice)
 
-    if p1_choice - comp_choice == 0:
+    winner = calculate_winner(p1_choice, comp_choice)
+
+    if winner == "draw":
         print("draw\n")
-    elif p1_choice - comp_choice == 4 or p1_choice - comp_choice == -2 and p1_choice < 4:
+    elif winner == "computer":
         cscor = cscor + 1
         print("computer wins\ncomputer:", cscor, "\nplayer 1:", pscor, "\n")
     else:
         pscor = pscor + 1
         print("player 1 wins\ncomputer:", cscor, "\nplayer 1:", pscor, "\n")
+
     loop()
 
 def bestof3(pscor, cscor):
-    p1_choice = d[input("player 1: ")]
-    comp_choice = computer_chooses()
+    p1_choice = input("player 1: ")
+    comp_choice = random.choice(hand)
+    print('computer:', comp_choice)
 
-    if p1_choice - comp_choice == 4 or p1_choice - comp_choice == -2 and p1_choice < 4:
+    winner = calculate_winner(p1_choice, comp_choice)
+    
+    if winner == "draw":
+        pass
+    elif winner == "computer":
         cscor = cscor + 1
     else:
         pscor = pscor + 1
+        
     if cscor==2 or pscor==2:
         if cscor==2:
             print('computer wins!')
@@ -66,15 +77,15 @@ def bestof3(pscor, cscor):
             sys.exit()
     bestof3(pscor, cscor)
 
-s = input(
+game_mode = input(
     "welcome to the rock-paper-scissors game\n"
     'instructions: enter "r" for rock, "p" for paper, and "s" for scissors\n\n'
     "for eternity game, type -> 1\nfor best of 3, type -> 2\n"
 )
 
-if s == "1":
+if game_mode == "1":
     loop()
-elif s == "2":
+elif game_mode == "2":
     bestof3(0, 0)
 else:
     print("invalid entry, run script again")
