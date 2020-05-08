ee09e10fee2da7cf175dae0fd178deae93013d5f
Henry Zhu <hi@henryzoo.com>
2020-05-08 15:48:59 -0400

    Allow input to also be lowercase + using # instead of s for sharp
    Co-authored-by: Braulio Lin <zupanssi@gmail.com>

--- interval.py
+++ interval.py
@@ -1,7 +1,7 @@
 #stepchart[] keys notes to value of semitones
-stepchart = {'C':0,'Cs':1,'Db':1,'D':2,'Ds':3,'Eb':3,'E':4,'Es':5,'Fb':4,
-             'F':5,'Fs':6,'Gb':6,'G':7,'Gs':8,
-             'Ab':8,'A':9,'Bb':10,'B':11,'Bs':12,'Cb':11}
+stepchart = {'C':0,'C#':1,'DB':1,'D':2,'D#':3,'EB':3,'E':4,'E#':5,'FB':4,
+             'F':5,'F#':6,'GB':6,'G':7,'G#':8,
+             'AB':8,'A':9,'BB':10,'B':11,'B#':12,'CB':11}
 #interval[] keys value of semitones to names of intervals
 interval={-11:'minor second',-10:'second',-9:'minor third',-8:'third',
           -7:'perfect fourth',-6:'tritone',
@@ -11,8 +11,10 @@ interval={-11:'minor second',-10:'second',-9:'minor third',-8:'third',
           4:'third',5:'perfect fourth',6:'tritone, augmented fourth or diminished fifth',7:'perfect fifth',
           8:'minor sixth or augmented fifth',9:'sixth',10:'minor seventh or augmented sixth',11:'seventh'}
 def func():
-    a=stepchart[input('Enter first note (tonic):')]
-    b=stepchart[input('Enter second note:')]
+    tonic=input('Enter first note (tonic): ')
+    note2=input('Enter second note: ')
+    a=stepchart[tonic.upper()]
+    b=stepchart[note2.upper()]
     v=b-a
     print(interval[v])
     func()
